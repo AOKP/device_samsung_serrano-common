5546766755bd0bd3180897e0def19f9eb939bfed
Esa Laukkanen <ele.linux@gmail.com>
2014-10-12 08:58:54 +0200

    Revert "serrano: Add custom RIL class"
    This reverts commit 80fd2e2744419e999e59043d1c6095321bab2eab.
    
    Change-Id: I3a40d095394fbe01155cf7bcee30c090a7cbde9d

--- serrano-common.mk
+++ serrano-common.mk
@@ -175,9 +175,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     qcom.hw.aac.encoder=true \
     ro.config.max_starting_bg=8
 
-# RIL
+# Enable Samsung EMS dial path
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=SamsungMSM8930RIL
+    ro.telephony.ril.v3=samsungEMSReq
 
 # call common msm8930
 $(call inherit-product, device/samsung/msm8930-common/msm8930.mk)
