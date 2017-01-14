c6fb6b75d0d58e16772c361afd396bbd9e7e4e02
Endre <ecseko@gmail.com>
2017-01-14 15:15:42 -0500

    Add AOKP Device Maintainer

--- system_prop.mk
+++ system_prop.mk
@@ -60,3 +60,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false \
     ro.am.reschedule_service=true \
     ro.sys.fw.dex2oat_thread_count=4
+
+#AOKP Device Maintainers
+ro.aokp.maintainer=csendre
